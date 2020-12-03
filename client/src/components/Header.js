f6ebd8f2f9d025878c3d58e5e533b78e07c1c889
rianyunandar <rianyunandar@gmail.com>
2020-12-03 10:20:16 +0700

    implement react router biar cepet

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,25 +6,25 @@
     
 import React from 'react'
 import {Container,Navbar,Nav} from 'react-bootstrap' 
+import {LinkContainer} from 'react-bootstrap'
+
 const Header = () => {
     return (
 <header>
     <Navbar bg="light" variant="light" expand="lg"  collapseOnSelect >
        <Container>
-        <Navbar.Brand href="/">
-                {/* <img 
+       <LinkContainer to='/'>                <img 
                     alt=""
-                    src="%PUBLIC_URL%/favicon-32x32.png"
+                    src="/favicon-32x32.png"
                     width="30"
                     height="30"
-                    className="d-inline-block align-top"/> */}
-                    Datu Sinar Jaya
-            </Navbar.Brand>
+                    className="d-inline-block align-top"/> Datu Sinar Jaya
+            </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                          <Nav.Link href="/cart"><i class="fas fa-shopping-cart"></i> Keranjang</Nav.Link>
-                        <Nav.Link href="login"><i class="fas fa-user"></i> Masuk</Nav.Link>
+                          <LinkContainer href="/cart"><i class="fas fa-shopping-cart"></i> Keranjang</LinkContainer>
+                        <LinkContainer href="login"><i class="fas fa-user"></i> Masuk</LinkContainer>
      
                     </Nav>
     
