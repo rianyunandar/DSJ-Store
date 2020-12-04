7e4d489d66e0bdb0eb33f9196eebb170caba15d3
rianyunandar <rianyunandar@gmail.com>
2020-12-04 17:33:56 +0700

    setup nodemon root project

--- client/src/screens/ProductScreen.jsx
+++ client/src/screens/ProductScreen.jsx
@@ -7,16 +7,17 @@ import Rating from '../components/Rating';
 const ProductScreen = ({match}) => {
 
 
-    const [product,setProduct]= useState({})
+    const [product, setProduct] = useState({})
 
-    useEffect(()=>{
-        const fetchProduct = async()=>{
-            const {data}=await axios.get(`/api/products/${match.params.id}`)
-
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [])
+    useEffect(() => {
+      const fetchProduct = async () => {
+        const { data } = await axios.get(`/api/products/${match.params.id}`)
+  
+        setProduct(data)
+      }
+  
+      fetchProduct()
+    }, [match])
     
     return (
         <>
